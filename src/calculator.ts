b983daad100e4afcb0ce1a5e0c7f2161e72f2300
contactpunit <contactpunitjain@gmail.com>
2024-10-30 21:11:56 +0530

    added code to pass testcase no. 4

--- src/calculator.ts
+++ src/calculator.ts
@@ -5,7 +5,7 @@ export function calculator(stringInput: CalcInput) {
     else {
         if (!stringInput.startsWith('//')) {
             const parts = stringInput.split(',')
-            const numbers = parts.map(part => +part)
+            const numbers = (parts.map(part => +part)).filter(num => !isNaN(num))
             return numbers.reduce((accum, currValue) => {
                 return accum + currValue
             })
