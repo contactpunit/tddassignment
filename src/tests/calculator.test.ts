5f9a8b91920558352d265cc7bf200eeb603fdfd3
contactpunit <contactpunitjain@gmail.com>
2024-10-30 19:11:25 +0530

    added testcase 2 for single number string arg

--- src/tests/calculator.test.ts
+++ src/tests/calculator.test.ts
@@ -4,4 +4,8 @@ describe('Calculator', () => {
     test('1. empty string return 0', () =>  {
         expect(Calculator("")).toBe(0)
     })
+
+    test("2. return single number which is passed as string", () => {
+        expect(Calculator("20")).toBe(20);
+    });
 })
